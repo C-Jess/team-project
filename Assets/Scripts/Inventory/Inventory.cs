7b10b71d6d8bdc03f8281ca8be6a3d5e37818119
DMoxom <moxomdaniel@gmail.com>
2022-03-16 17:59:51 +0000

    More Inventory/Organisation
    Inventory code foundation with test UI done. Some folder organisation for Inventory stuff

--- Assets/Scripts/Inventory/Inventory.cs
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -6,14 +6,18 @@ using UnityEngine;
 public class Inventory : ScriptableObject
 {
     public List<ItemObject> ItemList = new List<ItemObject>();
-     
+    
     public void AddItem(ItemObject item)
     {
         ItemList.Add(item);
     }
 
-    public void RemoveAt()
+    public void RemoveAt(int location)
     {
+        ItemList.RemoveAt(location);
+    }
 
+    private void OnEnable() {
+        ItemList.Clear();
     }
 }
